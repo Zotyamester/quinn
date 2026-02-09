2bb1945710da1a17308bb7f3aee9c4d4d001aa78
Zotyamester <szatmary.zoltan1222@gmail.com>
2026-02-09 23:46:18 +0100

    WIP: make administrative ECN mode configurable

--- quinn-proto/src/config/transport.rs
+++ quinn-proto/src/config/transport.rs
@@ -8,9 +8,42 @@ use qlog::streamer::QlogStreamer;
 #[cfg(feature = "qlog")]
 use crate::QlogStream;
 use crate::{
-    Duration, INITIAL_MTU, MAX_UDP_PAYLOAD, VarInt, VarIntBoundsExceeded, congestion,
-    connection::qlog::QlogSink,
+    Duration, EcnCodepoint, INITIAL_MTU, MAX_UDP_PAYLOAD, VarInt, VarIntBoundsExceeded, congestion, connection::qlog::QlogSink
 };
+
+/// Administrative mode of Explicit Congestion Notification (ECN)
+#[derive(Clone, Copy, Debug)]
+pub enum EcnMode {
+    /// Disable ECN completely
+    Disabled,
+    /// Classic ECN based on RFC3168
+    ///
+    /// Causes outgoing packets to be marked with ECT(0).
+    Classic,
+    /// ECN for Low Latency, Low Loss, Scalable throughput based on RFC9331
+    ///
+    /// Causes outgoing packets to be marked with ECT(1).
+    L4S,
+}
+
+impl EcnMode {
+    /// Whether the current ECN mode enables the use of ECN
+    pub fn is_enabled(&self) -> bool {
+        match self {
+            EcnMode::Disabled => false,
+            _ => true,
+        }
+    }
+
+    /// The corresponding ECN codepoint if ECN is enabled, `None` otherwise
+    pub fn codepoint(&self) -> Option<EcnCodepoint> {
+        match self {
+            EcnMode::Disabled => None,
+            EcnMode::Classic => Some(EcnCodepoint::Ect0),
+            EcnMode::L4S => Some(EcnCodepoint::Ect1),
+        }
+    }
+}
 
 /// Parameters governing the core QUIC state machine
 ///
@@ -51,7 +84,7 @@ pub struct TransportConfig {
     #[cfg(test)]
     pub(crate) deterministic_packet_numbers: bool,
 
-    pub(crate) enable_ecn: bool,
+    pub(crate) ecn_mode: EcnMode,
     pub(crate) congestion_controller_factory: Arc<dyn congestion::ControllerFactory + Send + Sync>,
 
     pub(crate) enable_segmentation_offload: bool,
@@ -313,8 +346,8 @@ impl TransportConfig {
     ///
     /// This allows congestion controllers to rely on ECN-CE marks (echoed by the remote peer
     /// through ECN counters) instead of drops for detecting congestion on the path.
-    pub fn enable_ecn(&mut self, value: bool) -> &mut Self {
-        self.enable_ecn = value;
+    pub fn enable_ecn(&mut self, value: EcnMode) -> &mut Self {
+        self.ecn_mode = value;
         self
     }
 
@@ -396,7 +429,7 @@ impl Default for TransportConfig {
             #[cfg(test)]
             deterministic_packet_numbers: false,
 
-            enable_ecn: true,
+            ecn_mode: EcnMode::Classic,
             congestion_controller_factory: Arc::new(congestion::CubicConfig::default()),
 
             enable_segmentation_offload: true,
@@ -432,7 +465,7 @@ impl fmt::Debug for TransportConfig {
             datagram_send_buffer_size,
             #[cfg(test)]
             deterministic_packet_numbers: _,
-            enable_ecn: send_ecn,
+            ecn_mode,
             congestion_controller_factory: _,
             enable_segmentation_offload,
             qlog_sink,
@@ -463,7 +496,7 @@ impl fmt::Debug for TransportConfig {
             .field("allow_spin", allow_spin)
             .field("datagram_receive_buffer_size", datagram_receive_buffer_size)
             .field("datagram_send_buffer_size", datagram_send_buffer_size)
-            .field("send_ecn", send_ecn)
+            .field("ecn_mode", ecn_mode)
             // congestion_controller_factory not debug
             .field("enable_segmentation_offload", enable_segmentation_offload);
         if cfg!(feature = "qlog") {
